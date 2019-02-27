166e8848650b82c8e7a8c83336cd87f604663cde
Arsenio Cantuangco Jr <abcantuangco@gmail.com>
2019-02-27 11:41:56 +0800

    Update dom-builder.js

--- dom-builder.js
+++ dom-builder.js
@@ -1,1 +1,49 @@
 
+const domBuilder = ( options ) => new Promise((resolve, reject) => {
+
+    const _build = ( options ) => {
+        let settings = {
+            element: null,
+            dom: null,
+            data: null,
+            buildType: 'html'
+        };
+
+        if ( typeof options !== 'undefined' )
+            Object.assign( settings, options );
+
+        return _buildView( settings );
+
+    };
+
+    const _buildView = ( options ) => new Promise((resolve, reject) => {
+        let $element = document.querySelector( options.element );
+        let $view = typeof options.dom === 'function' ? options.dom( options.data ) : false;
+
+        if (!$view)
+            throw new Error('DOM isn\'t available.');
+
+        switch ( options.buildType ) {
+            case 'html':
+            {
+                return new Promise((resolve, reject) => {
+                            $element.innerHTML = $view;
+                            resolve($view);
+                        });
+            }
+            break;
+            case 'append':
+            {
+                return new Promise((resolve, reject) => {
+                            $element.innerHTML += $view;
+                            resolve($view);
+                        });
+            }
+            break;
+        }
+    });
+
+    return {
+        build: build
+    };
+});
